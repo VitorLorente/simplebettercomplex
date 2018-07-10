d95191b51838e7f09d25ca3daf2708f09291a3b0
VitorLorente <lorente.sociologia@gmail.com>
2018-07-10 10:47:47 -0300

    preparando para o heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn projetinho.wsgi --log-file -
--- projetinho/settings.py
+++ projetinho/settings.py
@@ -136,3 +136,5 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+django_heroku.settings(locals())
